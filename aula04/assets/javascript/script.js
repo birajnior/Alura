638ab1ab2b69226d0d3be9a11a5176858fc271a7
birajnior <ubiratanbernardo@gmail.com>
2022-09-15 12:00:26 -0300

    Adicionando aula 04.

---
+++ aula04/assets/javascript/script.js
@@ -0,0 +1,11 @@
+
+let listaFilmes = ['Yesterday',' A chegada', ' Escola do Rock']
+
+listaFilmes.push(' Duro de Matar')
+
+
+for (let i = 0; i < 4; i ++) {
+    document.write('<p id="teste">' + i + '</p>')
+    document.write('<p id="teste">' + listaFilmes [i] + '</p>')
+    
+}
