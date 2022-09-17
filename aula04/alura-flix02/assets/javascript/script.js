a016a7faa761bd1da5d4a0414516797fd6905d5e
birajnior <ubiratanbernardo@gmail.com>
2022-09-17 19:56:07 -0300

    att.

--- aula04/alura-flix02/assets/javascript/script.js
+++ aula04/alura-flix02/assets/javascript/script.js
@@ -2,23 +2,22 @@ let lista_filmes = [
     'https://br.web.img2.acsta.net/pictures/17/09/29/21/15/4233147.jpg',
     'https://br.web.img2.acsta.net/medias/nmedia/18/89/43/82/20052140.jpg',
     'https://br.web.img3.acsta.net/pictures/17/10/02/19/20/5681659.jpg',
-    'https://cineclick-static.flixmedia.cloud/1280/69/1080x1620_1591280436.jpg',
-    'https://br.web.img3.acsta.net/medias/nmedia/18/87/89/83/20028679.jpg',
+    'https://cineclick-static.flixmedia.cloud/1280/69/1080x1620_1591191585.jpg',
     'https://cinema10.com.br/upload/series/series_1410_pswat.jpg',
     'https://media.fstatic.com/jWg862ksbIPOCPdYCK5UPtqZ9Ag=/210x312/smart/media/movies/covers/2021/10/wP9Ho6PCmmrFHLK1VjSAwZKVKtX.jpeg',
     'https://images.fanart.tv/fanart/station-19-5e51513235adc.jpg',
-    'https://upload.wikimedia.org/wikipedia/pt/b/bd/Click_film.jpg'
+    'https://upload.wikimedia.org/wikipedia/pt/b/bd/Click_film.jpg'    
 ]
 let listaNomes = [
     'The Flash',
     'Os Vingadores',
     'Arqueiro Verde',
-    'A Nova Onda do Imperador',
-    'Meu Malvado Favorito',
+    'Tropa de Elite',
     'S.W.A.T.',
     '9-1-1',
     'STATION 19',
     'CLICK'
+    
 ]
 
 document.getElementById('listaFilmes').innerHTML = ""
