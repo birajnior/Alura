6aa7cfce4dd08921659d91e131231ff5d3f1d7d7
birajnior <ubiratanbernardo@gmail.com>
2023-02-14 22:30:13 -0300

    Atualizando códigos JS.

--- introducao-javascript/src/js/principal.js
+++ introducao-javascript/src/js/principal.js
@@ -1,7 +1,7 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
-
 let pacientes = document.querySelectorAll('.paciente')
+let botaoAdicionar = document.querySelector('#adicionar-paciente')
 
 for (let i = 0; i < pacientes.length; i++) {
 
@@ -37,3 +37,41 @@ for (let i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(1)
     }
 }
+
+titulo.addEventListener("click", function () {
+    console.log('Também podemos chamar uma função anonima.');
+})
+
+botaoAdicionar.addEventListener("click", function (event) {
+    event.preventDefault()
+    console.log('Oi, eu sou o botão e fui clicado!!!');
+
+    let form = document.querySelector('#form-adiciona')
+
+    let nome = form.nome.value
+    let peso = form.peso.value
+    let altura = form.altura.value
+    let gordura = form.gordura.value
+
+    let pacienteTr = document.createElement('tr')
+
+    let nomeTd = document.createElement('td')
+    let pesoTd = document.createElement('td')
+    let alturaTd = document.createElement('td')
+    let gorduraTd = document.createElement('td')
+    let imcTd = document.createElement('td')
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    pacienteTr.appendChild(imcTd)
+
+    let tabela = document.querySelector('#tabela-pacientes')
+    tabela.appendChild(pacienteTr)
+})
